b79130c1f6fbd63e692a64a7b6fed42c7a370efc
taylor1119 <taylorcarter1119@gmail.com>
2024-09-04 07:06:05 +0100

    initial draft

---
+++ .prettierrc.mjs
@@ -0,0 +1,14 @@
+/** @type {import(prettier).Config} */
+export default {
+	plugins: ['prettier-plugin-astro'],
+	overrides: [{ files: '*.astro', options: { parser: 'astro' } }],
+
+	trailingComma: 'es5',
+	tabWidth: 4,
+	semi: false,
+	useTabs: true,
+	singleQuote: true,
+	jsxSingleQuote: true,
+	endOfLine: 'lf',
+	printWidth: 80,
+}
