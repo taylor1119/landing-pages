736bc894a5f7b8a7b3eb7432f576aa70aa2a3251
taylor1119 <taylorcarter1119@gmail.com>
2024-09-20 11:00:05 +0100

    updated packages

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,12 +3,9 @@ import prettier from 'eslint-config-prettier'
 import astro from 'eslint-plugin-astro'
 
 export default [
-	{ ignores: ['dist'] },
 	js.configs.recommended,
 	...astro.configs.recommended,
 	...astro.configs['jsx-a11y-recommended'],
 	prettier,
-	{
-		rules: { 'astro/jsx-a11y/anchor-is-valid': 'off' },
-	},
+	{ ignores: ['dist'] },
 ]
